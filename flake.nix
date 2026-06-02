1a97848ee7b9cc0947a415e57129f6b84d63d71a
MrKatzenGaming <max@langenhan2.de>
2026-06-02 15:58:23 +0200

    nix flake + envrc;

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    self.submodules = true;
+  };
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        build = import ./sys/nix pkgs;
+      in
+      {
+        formatter = pkgs.nixfmt-tree;
+        devShells.default = build.devShell "sys";
+      }
+    );
+}
